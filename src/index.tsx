fde2172a38548a4a1ac8d3db0071c929f85c6535
unknown <vlsova.irina.ru@yandex.ru>
2025-06-22 21:11:29 +0300

    Сделала публичным2

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,7 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// Состояние, в котором хранятся текущие параметры оформления статьи
 	const [articleState, setArticleState] =
 		useState<ArticleStateType>(defaultArticleState);
 	return (
